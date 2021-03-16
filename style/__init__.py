bb4a1b4fbb429050b556c1912b3f21f2d1472e2a
eduardo239 <eucrieiumaconta@gmail.com>
2021-03-16 18:47:59 -0300

    adicionado sistema de cards com imagens

--- style/__init__.py
+++ style/__init__.py
@@ -1,29 +1,22 @@
 primary = '#1F2C41'
 white = '#ffffff'
+dark = '#2c2c2c'
+light_grey = '#ddd'
+green = 'green'
 
 group_box = f"""padding: 0;"""
 
-lbl_simple = f"""  font-family: Arial;
+lbl_simple = f"""
   font-size: 12pt;
-  color: #2b2b2b;
-  text-transform: uppercase;
-  letter-spacing: 1px;
-  font-weight: bold;"""
+  color: {dark};
+  letter-spacing: 1px;"""
 
-lbl_score = f"""color: orange;"""
-lbl_fight_stats = f"""color: orange; background: '#2c2c2c'; padding: 2px 8px;"""
+lbl_fight_stats = f"""color: {dark}; padding: 5px;"""
 
 # btn
 btn_dark = f"""
   background-color: #141414;
   color: #eef0f2;
-  border: 0;
-  padding: 8px 16px;
-  font-style: uppercase;
-  letter-spacing: 1px;"""
-
-btn_primary = f"""background-color: #eec643;
-  color: #141414;
   border: 0;
   padding: 8px 16px;
   font-style: uppercase;
@@ -36,5 +29,18 @@ btn_secondary = f"""background-color: #ffffff;
   font-style: uppercase;
   letter-spacing: 1px;
 """
+lbl_winner = f"""font-size: 27pt; font-weight: bold; color: {green}"""
+lbl_score = f"""font-size: 27pt; color: {dark}"""
+lbl_stats = f"""font-size: 12pt; border: 1px solid {light_grey}; 
+background-color: transparent; padding: 5px 10px; background-color: {white}"""
+lbl_player_score = f"""font-size: 16pt; padding: 5px 10px;
+background-color: transparent; color: {primary}; border: 1px solid {light_grey}"""
+
+btn_primary = f"""background-color: #eec643;
+  color: #141414;
+  border: 0;
+  padding: 5px 15px;
+  font-style: uppercase;
+  letter-spacing: 1px;"""
 
 general = "QPushButton:pressed { background-color: red; border: 4px solid red; }"
